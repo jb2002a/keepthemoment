4d4ef45c32a5c24f938b9579bcac2e8ff5186645
jb2002a <2jaebin@gmail.com>
2026-07-19 19:07:50 +0900

    ,,

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,10 +2,12 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-project-id'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+const studioHost = process.env.SANITY_STUDIO_HOSTNAME || 'keepthemoment'
 
 export default defineCliConfig({
   api: {
     projectId,
     dataset,
   },
+  studioHost,
 })
